ea9bd111164de2d62dc90e6096cf191efb4a42bc
Helio Dourado <heliohdd@yahoo.com.br>
2025-04-01 19:35:22 -0300

    Header with Clerk Authentication and Landing Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ClientWrapper from "@/components/ClientWrapper";
+import Header from "@/components/Header";
+// import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClientWrapper>
+          <Header />
+
+          <main>{children}</main>
+
+          {/* <Toaster position="bottom-center" /> */}
+        </ClientWrapper>
       </body>
     </html>
   );
---
+++ components/ClientWrapper.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ClerkProvider } from "@clerk/nextjs";
+
+// import { SchematicProvider } from "@schematichq/schematic-react";
+// import SchematicWrapped from "./SchematicWrapped";
+// import { ConvexClientProvider } from "./ConvexClientProvider";
+
+export default function ClientWrapper({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  // const schematicPubKey = process.env.NEXT_PUBLIC_SCHEMATIC_PUBLISHABLE_KEY;
+  // if (!schematicPubKey) {
+  //   throw new Error(
+  //     "No Schematic Publishable Key found. Please add it to your .env.local file."
+  //   );
+  // }
+  return (
+    // <ConvexClientProvider>
+    //   <SchematicProvider publishableKey={schematicPubKey}>
+    //     <SchematicWrapped>
+    <ClerkProvider>{children}</ClerkProvider>
+    //       </SchematicWrapped>
+    //   </SchematicProvider>
+    // </ConvexClientProvider>
+  );
+}
