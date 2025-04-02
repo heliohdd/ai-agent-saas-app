6869b008c512e7c5889f4038d654f0e10ffd1a6b
Helio Dourado <heliohdd@yahoo.com.br>
2025-04-02 20:20:20 -0300

    Video Analysis Page Analyse Section

--- actions/analyseYoutubeVideo.ts
+++ actions/analyseYoutubeVideo.ts
@@ -1,14 +1,13 @@
 "use server";
 
-// import { getVideoIdFromUrl } from "@/lib/getVideoIdFromUrl";
+import { getVideoIdFromUrl } from "@/lib/getVideoIdFromUrl";
 import { redirect } from "next/navigation";
 
 export async function analyseYoutubeVideo(formData: FormData) {
   const url = formData.get("url")?.toString();
   if (!url) return;
 
-  const videoId = "abc"; // TODO: fix this
-  // const videoId = getVideoIdFromUrl(url);
+  const videoId = getVideoIdFromUrl(url);
 
   if (!videoId) return;
 
