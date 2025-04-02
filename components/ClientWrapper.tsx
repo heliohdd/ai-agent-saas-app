c5fd4ae3eb7086217f6e1b2d4b5187ed956567b6
Helio Dourado <heliohdd@yahoo.com.br>
2025-04-02 09:55:40 -0300

    Set up Schematic SDK for feature flagging & analytics

--- components/ClientWrapper.tsx
+++ components/ClientWrapper.tsx
@@ -2,7 +2,8 @@
 
 import { ClerkProvider } from "@clerk/nextjs";
 
-// import { SchematicProvider } from "@schematichq/schematic-react";
+import { SchematicProvider } from "@schematichq/schematic-react";
+import SchematicWrapped from "./SchematicWrapped";
 // import SchematicWrapped from "./SchematicWrapped";
 // import { ConvexClientProvider } from "./ConvexClientProvider";
 
@@ -11,19 +12,19 @@ export default function ClientWrapper({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const schematicPubKey = process.env.NEXT_PUBLIC_SCHEMATIC_PUBLISHABLE_KEY;
-  // if (!schematicPubKey) {
-  //   throw new Error(
-  //     "No Schematic Publishable Key found. Please add it to your .env.local file."
-  //   );
-  // }
+  const schematicPubKey = process.env.NEXT_PUBLIC_SCHEMATIC_PUBLISHABLE_KEY;
+  if (!schematicPubKey) {
+    throw new Error(
+      "No Schematic Publishable Key found. Please add it to your .env.local file."
+    );
+  }
   return (
     // <ConvexClientProvider>
-    //   <SchematicProvider publishableKey={schematicPubKey}>
-    //     <SchematicWrapped>
-    <ClerkProvider>{children}</ClerkProvider>
-    //       </SchematicWrapped>
-    //   </SchematicProvider>
+    <ClerkProvider>
+      <SchematicProvider publishableKey={schematicPubKey}>
+        <SchematicWrapped>{children}</SchematicWrapped>
+      </SchematicProvider>
+    </ClerkProvider>
     // </ConvexClientProvider>
   );
 }
