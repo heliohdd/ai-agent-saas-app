f98b17fbac1b75355ab8019d9988befc84e9a380
Helio Dourado <heliohdd@yahoo.com.br>
2025-04-04 16:54:36 -0300

    Vercel AI SDK and AI Agent Chat Interface

--- app/video/[videoId]/analysis/page.tsx
+++ app/video/[videoId]/analysis/page.tsx
@@ -1,45 +1,45 @@
 "use client";
 
-// import { createOrGetVideo } from "@/actions/createOrGetVideo";
+import { createOrGetVideo } from "@/actions/createOrGetVideo";
 // import AiAgentChat from "@/components/AiAgentChat";
 import YoutubeVideoDetails from "@/components/YoutubeVideoDetails";
 import ThumbnailGeneration from "@/components/ThumbnailGeneration";
 import TitleGenerations from "@/components/TitleGenerations";
 import Transcription from "@/components/Transcription";
 import Usage from "@/components/Usage";
-// import { Doc } from "@/convex/_generated/dataModel";
+import { Doc } from "@/convex/_generated/dataModel";
 import { FeatureFlag } from "@/app/features/flags";
 import { useUser } from "@clerk/nextjs";
 import { useParams } from "next/navigation";
-// import { useEffect, useState } from "react";
+import AiAgentChat from "@/components/AiAgentChat";
+import { useEffect, useState } from "react";
 
 function AnalysisPage() {
   const params = useParams<{ videoId: string }>();
   const { videoId } = params;
 
   const { user } = useUser();
-  // const [video, setVideo] = useState<Doc<"videos"> | null | undefined>(
-  //   undefined
-  // );
 
-  // useEffect(() => {
-  //   if (!user?.id) return;
+  const [video, setVideo] = useState<Doc<"videos"> | null | undefined>(
+    undefined
+  );
 
-  //   const fetchVideo = async () => {
-  //     // Analyse the video (add video to db here)
-  //     const response = await createOrGetVideo(videoId as string, user.id);
-  //     if (!response.success) {
-  //       // toast.error("Error creating or getting video", {
-  //       //   description: response.error,
-  //       //   duration: 10000,
-  //       // });
-  //     } else {
-  //       setVideo(response.data!);
-  //     }
-  //   };
-
-  //   fetchVideo();
-  // }, [videoId, user]);
+  useEffect(() => {
+    if (!user?.id) return;
+    const fetchVideo = async () => {
+      // Analyse the video (add video to db here)
+      const response = await createOrGetVideo(videoId as string, user.id);
+      if (!response.success) {
+        //       // toast.error("Error creating or getting video", {
+        //       //   description: response.error,
+        //       //   duration: 10000,
+        //       // });
+      } else {
+        setVideo(response.data!);
+      }
+    };
+    fetchVideo();
+  }, [videoId, user]);
 
   // const VideoTranscriptionStatus =
   //   video === undefined ? (
@@ -99,7 +99,7 @@ function AnalysisPage() {
         {/* Right Side */}
         <div className="order-1 lg:order-2 lg:sticky lg:top-20 h-[500px] md:h-[calc(100vh-6rem)]">
           {/* Ai Agent Chat Section */}
-          {/* TODO          <AiAgentChat videoId={videoId} /> */}
+          <AiAgentChat videoId={videoId} />
         </div>
       </div>
     </div>
